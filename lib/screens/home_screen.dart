3de676fd091d5509ed84d70ef2806e1565b92cc8
winsongr <winsongr08@gmail.com>
2022-09-23 22:38:15 +0530

    Clean

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -71,29 +71,7 @@ class _HomeScreenState extends State<HomeScreen> {
       }
     });
   }
-  // firebaseMessaging.configure(onMessage: (Map<String, dynamic> message) {
-  //   print('onMessage: $message');
-  //   Platform.isAndroid
-  //       ? showNotification(message['notification'])
-  //       : showNotification(message['aps']['alert']);
-  //   return;
-  // }, onResume: (Map<String, dynamic> message) {
-  //   print('onResume: $message');
-  //   return;
-  // }, onLaunch: (Map<String, dynamic> message) {
-  //   print('onLaunch: $message');
-  //   return;
-  // });
 
-  // firebaseMessaging.getToken().then((token) {
-  //   print('token: $token');
-  //   Firestore.instance
-  //       .collection('users')
-  //       .document(currentUserId)
-  //       .updateData({'pushToken': token});
-  // }).catchError((err) {
-  //   Fluttertoast.showToast(msg: err.message.toString());
-  // });
   @override
   void initState() {
     super.initState();
